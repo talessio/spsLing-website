7d7b66ebe7edb334aabd4b461fb10c2d7a3d5bd2
talessio <81319268+talessio@users.noreply.github.com>
2021-10-02 11:45:32 +0200

    deleted comments

--- components/date.js
+++ components/date.js
@@ -1,9 +1,6 @@
 import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
-    // if (!dateString) {
-    //     return '';
-    // }
     const date = parseISO(dateString)
     return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
